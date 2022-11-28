7f5721eb23b328266327deaed7336438256cc0ec
IlimaG <ivanilimag@gmail.com>
2022-11-28 18:33:01 +0100

    Examen JS

---
+++ JS/form.js
@@ -0,0 +1,46 @@
+
+const form = document.querySelector("#form")
+const nameInput = document.querySelector("#name")
+const mailInput = document.querySelector("#mail")
+const telInput = document.querySelector("#tel")
+const privacyInput = document.querySelector("#privacy")
+
+const formValidation = (e) => {
+    e.preventDefault()
+
+    if (!nameInput.value){
+        alert('Introduce un nombre valido.')
+        nameInput.focus()
+        return
+    }
+
+    if (nameInput.value.length < 3) {
+        alert('El nombre tiene que tener 3 o mas catacteres ')
+        nameInput.focus()
+        return
+    }
+
+    function validateEmail(email) {
+        let validate = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return validate.test(email) ? true : false;
+    }
+
+    if (!validateEmail(mailInput.value)) {
+        alert('Introduce un email valido')
+        mailInput.focus()
+        return
+    }
+    if (!(telInput.value * 1) || telInput.value.length < 9) {
+        alert('Introduce un número de teléfono valido')
+        telInput.focus()
+        return
+    }
+
+    let checked = privacyInput.checked
+    if (!checked) {
+        alert('Acepta la Política de Privacidad y los Términos y condiciones')
+        return
+    }
+}
+
+form.addEventListener('submit', formValidation)
--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,4 @@
 
-const tarrazú = document.querySelector("#btnTarrazu")
-const losNaranjos = document.querySelector("#btnLosNaranjos")
-const laosAmanecer = document.querySelector("#btnLaosAmanecer")
-const etiopíaYrgacheff = document.querySelector("#btnEtiopíaYrgacheff")
-const shopCart = document.querySelector("#shopCart")
-
 localStorage.clear()
 
 let coffeStorage = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
@@ -18,104 +12,235 @@ product = [
         idProduct: 1,
         name: "Costa Rica Tarrazú",
         price: 9.00,
-        stock: 5,
-        quantity: 0
+        quantity: 0,
+        imagen: `<img src="./assets/img/Images.png" alt="Costa Rica Tarrazú" class="imgCartShop">`
     },
     {
         idProduct: 2,
         name: "Colombia Los Naranjos",
         price: 9.00,
-        stock: 8,
-        quantity: 0
+        quantity: 0,
+        imagen: `<img src="./assets/img/Images (1).png" alt="Colombia Los Naranjos" class="imgCartShop"> `
+
     },
     {
         idProduct: 3,
         name: "Laos Amanecer",
         price: 9.00,
-        stock: 3,
-        quantity: 0
+        quantity: 0,
+        imagen: `<img src="./assets/img/Images (2).png" alt="Laos Amanecer" class="imgCartShop">`
     },
     {
         idProduct: 4,
         name: "Etiopía Yrgacheff",
         price: 9.00,
-        stock: 7,
-        quantity: 0
+        quantity: 0,
+        imagen: `<img src="./assets/img/Images (3).png" alt="Etiopía Yrgacheff" class="imgCartShop"> `
+
     }
 ]
 
-// AÑADIR CAFE SELECCIONADO AL LOCAL STORE 
+// AÑADIR CAFE SELECCIONADO AL LOCAL STORE
+
+const tarrazú = document.querySelector("#btnTarrazu")
+const losNaranjos = document.querySelector("#btnLosNaranjos")
+const laosAmanecer = document.querySelector("#btnLaosAmanecer")
+const etiopíaYrgacheff = document.querySelector("#btnEtiopíaYrgacheff")
+const cest = document.querySelector("#cest")
 
 const newCoffe = (button) => {
     switch (button) {
         case tarrazú:
             product[0].quantity++
             coffeStorage.push(product[0])
-            localStorage.setItem('cart', JSON.stringify(coffeStorage))
+            cest.style.display = "flex"
             break;
         case losNaranjos:
             product[1].quantity++
             coffeStorage.push(product[1])
-            localStorage.setItem('cart', JSON.stringify(coffeStorage))
+            cest.style.display = "flex"
             break;
         case laosAmanecer:
             product[2].quantity++
             coffeStorage.push(product[2])
-            localStorage.setItem('cart', JSON.stringify(coffeStorage))
+            cest.style.display = "flex"
             break;
         case etiopíaYrgacheff:
             product[3].quantity++
             coffeStorage.push(product[3])
-            localStorage.setItem('cart', JSON.stringify(coffeStorage))
+            cest.style.display = "flex"
             break;
         default:
             break;
+
+            
     }
+
     noDuplicates(button)
 }
 
+       
 
-// FUNCION NO DUPLICADOS 
+// FUNCION ACTUALIZAR LOCAL STORAGE / IMPRIMiR CESTA / PRODUCTOS TOTALES 
+
+const shopCart = document.querySelector("#shopCart")
+let total = document.querySelector("#total")
+const totalProducts = document.querySelector("#totalProducts")
 
 function noDuplicates() {
     shopCart.innerHTML = ''
-
 
     let noDuplicates = coffeStorage.filter((item, index) => {
         return coffeStorage.indexOf(item) === index;
     })
 
     noDuplicates.forEach(e => {
-        shopCart.innerHTML += `<div><h5>${e.name}</h5>${e.price}€ <button>+</button>${e.quantity}<button onclick = "deleteNote()">-</button> <br> <p> Total: ${e.price * e.quantity} €</p> </div>`
+
+        if (e.quantity === 0) {
+            noDuplicates.splice(noDuplicates.indexOf(e), 1)
+        }
     })
 
+
+    let price = 0
+    let totalsProducts = 0
+
+
+    noDuplicates.forEach( e=> {
+        shopCart.innerHTML += `<div id="product">${e.imagen}<div><div id="id">${e.idProduct}</div><h5>${e.name}</h5><p id="priceU">Precio unidad: ${e.price}€</p> <button id="add" onclick = "add(this)">+</button><span id="cuantity">${e.quantity}</span>
+        <button id="minus" onclick = "minus(this)">-</button> <button id="deleteProduct" onclick = "deleteProduct(this)">X </button> <p> Total: ${e.price * e.quantity} €  </p></div> </div>`
+        price += (e.price * e.quantity)
+        totalsProducts += e.quantity
+    })
+
+    shopCart.innerHTML += `<p id="subTotal">Subtotal (${totalsProducts} productos): ${price} € </p>`
+
+    if (totalsProducts === 0) {
+        totalProducts.style.display = "none"
+    } else {
+        totalProducts.innerHTML = totalsProducts
+        totalProducts.style.display = "inline"
+    }
+    
+
+    if (noDuplicates.length <= 0) {
+        cest.style.display = "none"
+        shopCart.innerHTML = `<div id="emptyCart"> La cesta esta vacia</div>`
+    }
+    
     coffeStorage = noDuplicates
     localStorage.setItem('cart', JSON.stringify(coffeStorage))
 }
 
-// BOTON MENOS 
+// BOTON QUITAR PRODUCTO
 
-const deleteNote = (button) => {
+const minus = (button) => {
 
+    let minusCoffe = button.parentNode.textContent
+    let arr = minusCoffe
+    arr = [...arr]
 
+    if (arr[0] == '1') {
+        if(product[0].quantity === 0){
+            return
+        }
+        product[0].quantity--
+    }
+    if (arr[0] == "2") {
+        if(product[1].quantity === 0){
+            return
+        }
+        product[1].quantity--
+    }
+    if (arr[0] == "3") {
+        if(product[2].quantity === 0){
+            return
+        }
+        product[2].quantity--
+    }
+    if (arr[0] == "4") {
+        if(product[3].quantity === 0){
+            return
+        }
+        product[3].quantity--
+    }
 
-
-
-    noDuplicates(button)
+    noDuplicates()
 }
 
+// BOTON AÑADIR PRODUCTO
+
+const add = (button) => {
+
+    let addCoffe = button.parentNode.textContent
+    let arr = addCoffe
+    arr = [...arr]
+
+    if (arr[0] == '1') {
+        product[0].quantity++
+    }
+    if (arr[0] == "2") {
+        product[1].quantity++
+    }
+    if (arr[0] == "3") {
+        product[2].quantity++
+    }
+    if (arr[0] == "4") {
+        product[3].quantity++
+    }
+
+    noDuplicates()
+
+}
+
+// BORRAR CESTA ENTERA 
+
+const deleteAll = document.querySelector("#deleteAll")
+
+deleteAll.onclick = (e) => {
+    e.preventDefault()
+    coffeStorage = []
+    noDuplicates()
+    shopCart.innerHTML = `<div id="emptyCart"> La cesta esta vacia</div>`
+    cest.style.display = "none"
+}
 
 // MOSTRAR/OCULTAR CESTA
 
 const cart = document.querySelector("#cart")
+const shop = document.querySelector("#shop")
 
 cart.onclick = (e) => {
     e.preventDefault()
-    if (shopCart.style.display === "none") {
-        shopCart.style.display = "block"
+    if (shop.style.display === "none") {
+        shop.style.display = "block"
     } else {
-        shopCart.style.display = "none"
+        shop.style.display = "none"
     }
 }
 
+// ELIMINAR ESE PRODUCTO 
+
+const deleteProduct = (button) => {
+
+    let deleteThisCoffe = button.parentNode.textContent
+    let arr = deleteThisCoffe
+    arr = [...arr]
+    id = arr[0]
+console.log(deleteThisCoffe);
+console.log(arr);
+console.log(id);
+
+    let deleteProduct = coffeStorage
+    deleteProduct.forEach(e => {
+        
+        if (id == e.idProduct) {
+            deleteProduct.splice(deleteProduct.indexOf(e), 1)
+            e.quantity = 0
+        }
+    })
+
+    arr = []
+    noDuplicates()
+}
 
